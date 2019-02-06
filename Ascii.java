538818869dc712b0e0e5f08c8d6c4b2cc44a35cf
christinajoice <christinallrejoice@gmail.com>
2019-02-05 17:31:37 -0800

    Create Ascii.java

---
+++ Ascii.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+public class Ascii
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+System.out.rpintln("Enter a character")l
+Character c=s.nextChar();
+int ascii=c;
+System.out.println("Ascii value is"+ascii);
+}
+}
