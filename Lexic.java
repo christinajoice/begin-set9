509a5e2e993166b78bdcb5ef6e81d2c01352676f
christinajoice <christinallrejoice@gmail.com>
2019-02-07 11:10:04 +0530

    Create Lexic.java

---
+++ Lexic.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class Lexic
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter a string");
+String str=s.nextLine();
+for(int i=0;i<str.length();i++)
+{
+
+}
+}
+]
