793f709814f108afc2cdf6d4bd1a37a29274ac16
christinajoice <christinallrejoice@gmail.com>
2019-03-01 13:57:07 +0530

    Update and rename Lexic.java to strtoop.py

---
+++ strtoop.py
@@ -0,0 +1,17 @@
+str=input("Enter a string")
+li=str.split(" ")
+num1=int(li[0])
+num2=int(li[2])
+op=li[1]
+if op=='+':
+  result=num1+num2
+elif op=='-':
+  result=num1-num2
+elif op=='*':
+  result=num1*num2
+elif op=='/':
+  result=num1/num2
+elif op=='%':
+  result=num1+num2
+print (int(esult))
+  
