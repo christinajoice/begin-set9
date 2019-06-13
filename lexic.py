69031d474ff001bbf29b4c2b0aa911224a8d0580
christinajoice <christinallrejoice@gmail.com>
2019-06-13 13:49:23 +0530

    Update lexic.py

--- lexic.py
+++ lexic.py
@@ -1,4 +1,4 @@
-str1=input("Enter a string")
+str1=input()
 li=list(str1)
 for i in range(len(li)):
   for j in range(len(li)):
@@ -6,4 +6,4 @@ for i in range(len(li)):
       temp=li[i]
       li[i]=li[j]
       li[j]=temp
-print(li.join())
+print(''.join(li))
